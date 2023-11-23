ea4ec47d5172cd00f00a4e160d6d5a98806c46cc
mk1-p <mk1.dev662@gmail.com>
2023-11-23 16:43:08 +0900

    ENH: Add ManyToMany Relation

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,18 @@ import { UserModel } from "./entity/user.entity";
 import { StudentModel, TeacherModel } from "./entity/person.entity";
 import { ProfileModel } from "./entity/profile.entity";
 import { PostModel } from "./entity/post.entity";
+import { TagModel } from "./entity/tag.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserModel, ProfileModel, PostModel]),
+    TypeOrmModule.forFeature([UserModel, ProfileModel, PostModel, TagModel]),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: '127.0.0.1',
       username: 'postgres',
       password: 'postgres',
       database: 'typeormstudy',
-      entities: [UserModel, StudentModel, TeacherModel, ProfileModel, PostModel],
+      entities: [UserModel, StudentModel, TeacherModel, ProfileModel, PostModel, TagModel],
       synchronize: true,
     }),
   ],
--- src/entity/post.entity.ts
+++ src/entity/post.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { UserModel } from "./user.entity";
+import { TagModel } from "./tag.entity";
 
 @Entity()
 export class PostModel{
@@ -13,6 +14,19 @@ export class PostModel{
    */
   @ManyToOne(() => UserModel, (user) => user.posts)
   author: UserModel;
+
+  /**
+   * @ManyToMany
+   * N:M 관계를 나타낸다.
+   * @JoinTable Annotation을 추가해줌으로써 중간 테이블을 만들어야한다.
+   * 연관관계 둘중 하나만 만듷어주면된다.
+   * Spring JPA 에서는 ManyToMany 를 실무에서 사용하지 않도록 하는데...
+   * 작동 방식 및 순환참조의 위험성이 없는지 체크해보자
+   */
+  @ManyToMany(() => TagModel, (tag) => tag.posts)
+  @JoinTable()
+  tags: TagModel[];
+
   @Column()
   title: string;
 }
